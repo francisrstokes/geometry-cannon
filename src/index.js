19661bab98f3cc326e59f3a94da91ac6f262c7c0
Frank Stokes <frank@wearereasonablepeople.com>
2017-12-01 12:02:01 +0100

    Separate tweet logic, add interval based automatic posting

--- src/index.js
+++ src/index.js
@@ -1,56 +1,11 @@
-const config = require('./config')
-const Twit = require('twit');
-const T = new Twit(config);
 const generate = require('./generate');
+const tweet = require('./tweet');
 
-generate().then(res => {
-  const { image, type, params } = res;
+if (process.argv.length >= 3 && process.argv[2] === '--post-initial') {
+  generate().then(tweet);
+}
 
-  /* eslint-disable camelcase */
-  const metadataCreateCb = (mediaIdStr) =>
-  (err, data) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Done creating metadata...');
-      const tweetStr = `${type}:${JSON.stringify(params).replace(/"/g, '')}`;
-      console.log(tweetStr)
-      const tweet = {
-        status: tweetStr,
-        media_ids: [
-          mediaIdStr
-        ]
-      };
-
-      console.log('Posting tweet...');
-      T.post('statuses/update', tweet, (err, data) => {
-        if (err) {
-          console.log('Something went wrong while posting!');
-          console.log(err);
-        } else {
-          console.log('Success!');
-        }
-      });
-    }
-  };
-
-  const mediaUploadCb = (err, data) => {
-  if (err) {
-    console.log(err);
-  } else {
-    console.log('Done uploading.');
-    const mediaIdStr = data.media_id_string;
-    const meta_params = {
-      media_id: mediaIdStr,
-      alt_text: { text: 'Generated Image' }
-    };
-
-    console.log('Creating metadata...');
-    T.post('media/metadata/create', meta_params, metadataCreateCb(mediaIdStr));
-  }
-  };
-
-  console.log('Starting upload');
-  T.post('media/upload', { media_data: image }, mediaUploadCb);
-  /* eslint-disable camelcase */
-});
+const FOUR_HOURS = 1000 * 60 * 4;
+setInterval(() => {
+  generate().then(tweet);
+}, FOUR_HOURS);
